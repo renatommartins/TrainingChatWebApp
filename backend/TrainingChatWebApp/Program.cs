78972a3ec017d5bba9032180fef40d84437e3822
Renato <renato.matosinho.martins@gmail.com>
2023-08-15 19:10:26 -0300

    fix: add missing origins to CORS

--- backend/TrainingChatWebApp/Program.cs
+++ backend/TrainingChatWebApp/Program.cs
@@ -13,7 +13,7 @@ internal static class Program
 				name:AllowedOrigins,
 				policy =>
 				{
-					policy.WithOrigins("http://localhost:5140");
+					policy.WithOrigins("http://localhost:5140","http://localhost:5500","http://127.0.0.1:5500");
 					policy.WithHeaders("Authorization");
 				});
 		});
