57bdf26a55fafac46a868e18bba0c23d6a9dcf96
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-08-17 18:38:11 -0300

    feat: localstorage session and chat page redirect

--- frontend/login.js
+++ frontend/login.js
@@ -1,17 +1,33 @@
 function login() {
     let user = document.getElementById("inUser").value;
-    let password = document.getElementById("inPassword").value;
-
+    let passwordElement = document.getElementById("inPassword");
+    let password = passwordElement.value
+    let outAlert = document.getElementById("outAlert");
     console.log(user, password);
     let encodedLogin = "Basic " + btoa(user + ":" + password);
 
     console.log(encodedLogin);
 
-    const httpRequest = new XMLHttpRequest();
-    const url = 'http://localhost:5140/login';
+    let httpRequest = new XMLHttpRequest();
+    let url = 'http://localhost:5140/login';
     httpRequest.open("GET", url);
-    httpRequest.onreadystatechange = function (e) {
-        console.log(httpRequest.responseText)
+    httpRequest.onloadend = function (e) {
+        switch (httpRequest.status) {
+            case 200:
+                console.log("Enviar para a página correta");
+                let session = JSON.parse(httpRequest.response).sessionId;
+                myStorage = localStorage;
+                localStorage.setItem(myStorage, session);
+                window.location.href = "/frontend/chat.html"
+                break;
+            case 401:
+                passwordElement.value = '';
+                outAlert.innerHTML = "Vai esquecer a senha no caralho";
+                console.log("Erro de senha ou usuário");
+                break;
+            case 500:
+                console.log("servidor bunda mole, tente novamente");
+        }
     };
     httpRequest.setRequestHeader(
         "Authorization",
