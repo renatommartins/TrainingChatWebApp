bf2c46376a0774de1a87a08801fcd34ded4977bf
Renato <renato.matosinho.martins@gmail.com>
2023-10-03 19:15:45 -0300

    feat: add svelte support

---
+++ backend/TrainingChatWebApp/js/vite.config.js
@@ -0,0 +1,17 @@
+﻿import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build:{
+        rollupOptions:{
+            input:{
+                app: "./index_test.html"
+            },
+            output:{
+                dir: "../wwwroot-nocache/"
+            }
+        }
+    },
+    plugins: [svelte()],
+})
